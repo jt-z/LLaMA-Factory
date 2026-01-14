0522cc37b8fd3f494b5067b82f33b6790fe80b3b
jt-z <jianting112358@gmail.com>
2026-01-14 17:08:24 +0800

    8卡3090上LoRA微调LLama3 8B，可以把单卡批次大小开到8，主要线性增长的是中间激活值和 微调的LoRA的权重矩阵里的少量参数

--- self_train_config/llama3_lora_sft_232_8GPU.sh
+++ self_train_config/llama3_lora_sft_232_8GPU.sh
@@ -2,8 +2,10 @@
 
 set -x
 
-MODEL_PATH=meta-llama/Meta-Llama-3-8B-Instruct
+MODEL_PATH=/home/ksa/.cache/modelscope/hub/models/LLM-Research/Meta-Llama-3-8B-Instruct
 
+
+# 8卡3090上LoRA微调LLama3 8B，可以把单卡批次大小开到8，主要线性增长的是中间激活值和 微调的LoRA的权重矩阵里的少量参数 
 llamafactory-cli train \
     --model_name_or_path ${MODEL_PATH} \
     --trust_remote_code \
@@ -12,21 +14,21 @@ llamafactory-cli train \
     --finetuning_type lora \
     --lora_rank 8 \
     --lora_target all \
-    --dataset identity,alpaca_en_demo \
+    --dataset chatml_alpaca_gpt4_zh \
     --template llama3 \
     --cutoff_len 2048 \
     --max_samples 1000 \
     --overwrite_cache \
     --preprocessing_num_workers 16 \
     --dataloader_num_workers 4 \
-    --output_dir saves/llama3-8b/lora/sft \
+    --output_dir saves/llama3-8b/lora/sft_chatml_alpaca_gpt4_zh \
     --logging_steps 10 \
     --save_steps 500 \
     --plot_loss \
     --overwrite_output_dir \
     --save_only_model false \
     --report_to none \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 8 \
     --learning_rate 1e-4 \
     --num_train_epochs 3.0 \
