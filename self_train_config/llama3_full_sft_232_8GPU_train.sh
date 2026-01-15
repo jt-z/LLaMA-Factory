45ae830bf754afb189017376240e6c940604b8c6
jt-z <jianting112358@gmail.com>
2026-01-15 15:36:17 +0800

    用不同的启动配置来进行FT训练测试

--- self_train_config/llama3_full_sft_232_8GPU_train.sh
+++ self_train_config/llama3_full_sft_232_8GPU_train.sh
@@ -4,4 +4,4 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # 运行命令：指定 YAML 文件路径
-llamafactory-cli train self_train_config/llama3_full_sft_232_8GPU.yaml
+llamafactory-cli train self_train_config/llama3_full_sft_232_8GPU_same_biren.yaml
